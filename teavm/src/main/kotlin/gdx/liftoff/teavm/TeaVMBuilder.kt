afc6a2ff404b92cc12ed3f3f3b4f8be87a427771
tommyettinger <tommy.ettinger@gmail.com>
2024-08-15 11:34:40 +0000

    Automatic deployment: gdx-liftoff 1.12.1.13.

--- teavm/src/main/kotlin/gdx/liftoff/teavm/TeaVMBuilder.kt
+++ teavm/src/main/kotlin/gdx/liftoff/teavm/TeaVMBuilder.kt
@@ -1,17 +1,19 @@
 package gdx.liftoff.teavm
 
 import java.io.File
+import com.github.xpenatan.gdx.backends.teavm.config.AssetFileHandle
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder
 import com.github.xpenatan.gdx.backends.teavm.config.plugins.TeaReflectionSupplier
 import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass
+import org.teavm.vm.TeaVMOptimizationLevel
 
 /** Builds the TeaVM/HTML application. */
 @SkipClass
 object TeaVMBuilder {
     @JvmStatic fun main(arguments: Array<String>) {
         val teaBuildConfiguration = TeaBuildConfiguration().apply {
-            assetsPath.add(File("../assets"))
+            assetsPath.add(AssetFileHandle("../assets"))
             webappPath = File("build/dist").canonicalPath
             // Register any extra classpath assets here:
             // additionalAssetsClasspathFiles += "gdx/liftoff/asset.extension"
@@ -22,6 +24,8 @@ object TeaVMBuilder {
 
         val tool = TeaBuilder.config(teaBuildConfiguration)
         tool.mainClass = "gdx.liftoff.teavm.TeaVMLauncher"
+        tool.optimizationLevel = TeaVMOptimizationLevel.FULL
+        tool.setObfuscated(true)
         TeaBuilder.build(tool)
     }
 }
