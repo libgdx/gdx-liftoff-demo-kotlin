131575ecc9cc5ca24ce846638e536749b9f9543a
tommyettinger <tommy.ettinger@gmail.com>
2025-03-25 10:07:38 +0000

    Automatic deployment: gdx-liftoff 1.13.1.3.

--- lwjgl3/src/main/kotlin/gdx/liftoff/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/gdx/liftoff/lwjgl3/Lwjgl3Launcher.kt
@@ -13,7 +13,19 @@ fun main() {
       return
     Lwjgl3Application(Main(), Lwjgl3ApplicationConfiguration().apply {
         setTitle("gdx-liftoff-demo-kotlin")
+        //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
+        //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
+        useVsync(true)
+        //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
+        //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
+        setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1)
+        //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
+        //// useful for testing performance, but can also be very stressful to some hardware.
+        //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+
         setWindowedMode(640, 480)
+        //// You can change these files; they are in lwjgl3/src/main/resources/ .
+        //// They can also be loaded from the root of assets/ .
         setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
     })
 }
