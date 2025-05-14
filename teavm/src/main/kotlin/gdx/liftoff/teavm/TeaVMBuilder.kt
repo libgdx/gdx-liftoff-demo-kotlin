545d36e4a5d35bceca9eaa19e7878f7af95ae898
tommyettinger <tommy.ettinger@gmail.com>
2025-05-14 03:18:00 +0000

    Automatic deployment: gdx-liftoff 1.13.5.0.

--- teavm/src/main/kotlin/gdx/liftoff/teavm/TeaVMBuilder.kt
+++ teavm/src/main/kotlin/gdx/liftoff/teavm/TeaVMBuilder.kt
@@ -5,11 +5,10 @@ import com.github.xpenatan.gdx.backends.teavm.config.AssetFileHandle
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuildConfiguration
 import com.github.xpenatan.gdx.backends.teavm.config.TeaBuilder
 import com.github.xpenatan.gdx.backends.teavm.config.plugins.TeaReflectionSupplier
-import com.github.xpenatan.gdx.backends.teavm.gen.SkipClass
+import org.teavm.tooling.TeaVMTargetType
 import org.teavm.vm.TeaVMOptimizationLevel
 
 /** Builds the TeaVM/HTML application. */
-@SkipClass
 object TeaVMBuilder {
     @JvmStatic fun main(arguments: Array<String>) {
         val teaBuildConfiguration = TeaBuildConfiguration().apply {
@@ -23,9 +22,16 @@ object TeaVMBuilder {
         // TeaReflectionSupplier.addReflectionClass("gdx.liftoff.reflect")
 
         val tool = TeaBuilder.config(teaBuildConfiguration)
+        // You can uncomment the line below to use WASM instead of JavaScript as a target.
+        // Some code can see very significant performance benefits from WASM, and some won't.
+//        tool.targetType = TeaVMTargetType.WEBASSEMBLY_GC
         tool.mainClass = "gdx.liftoff.teavm.TeaVMLauncher"
+        // For many (or most) applications, using a high optimization won't add much to build time.
+        // If your builds take too long, and runtime performance doesn't matter, you can change ADVANCED to SIMPLE .
         tool.optimizationLevel = TeaVMOptimizationLevel.ADVANCED
-        tool.setObfuscated(true)
+        // The line below should use tool.setObfuscated(false) if you want clear debugging info.
+        // You can change it to tool.setObfuscated(true) when you are preparing to release, to try to hide your original code.
+        tool.setObfuscated(false)
         TeaBuilder.build(tool)
     }
 }
